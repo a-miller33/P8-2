7a1ec422117e9090c762ec6425d4136950db5856
Adam Miller <a-miller.33@onu.edu>
2024-10-24 14:05:44 -0400

    completed countWords function

--- P8-2/P8-2.cpp
+++ P8-2/P8-2.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int countWords(string str);
+
+int main() {
+	string str;
+	cout << "Enter a string: ";
+	getline(cin, str);
+	cout << "Word count: " << countWords(str) << endl;
+
+	return 0;
+}
+
+int countWords(string str) {
+	int count = 1;
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == ' ' && str[i + 1] != ' ') {
+			count++;
+		}
+	}
+	return count;
+}
